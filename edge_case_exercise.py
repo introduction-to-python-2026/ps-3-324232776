6d726ad9de6aff7a6321ae89bee911d51f138105
324232776 <ledoronlevi@gmail.com>
2025-11-11 20:01:45 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,12 +1,11 @@
 def move(my_list, direction = None):
-   index_of_one = my_list.index(1)
+   index_of_one=my_list.index(1) # Store the index of 1 once
    if direction == 'right' and index_of_one != len(my_list) - 1:
-     my_list[my_list.index(1)] = 0
-     my_list[my_list.index(1) + 1] = 1
+     my_list[index_of_one] = 0 # Use the stored index
+     my_list[index_of_one + 1] = 1 # Use the stored index
 
-    elif direction == 'left' and index_of_one != 0:
-      my_list[my_list.index(1)] = 0
-      my_list[my_list.index(1) - 1] = 1
+   elif direction == 'left' and index_of_one != 0:
+      my_list[index_of_one] = 0 # Use the stored index
+      my_list[index_of_one - 1] = 1 # Use the stored index
 
-    
-    return my_list
+   return my_list
